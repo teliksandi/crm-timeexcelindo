4598588f5096bf9b7ef3dc74a55ef9cf0b0b956d
DESKTOP-O9GFHDK\Lathiif Aji Santhoso <latif_cf@yahoo.co.id>
2017-10-16 10:45:30 +0700

    controlller

--- resource/cismart/application/controllers/monitoring/monitoring.php
+++ resource/cismart/application/controllers/monitoring/monitoring.php
@@ -11,6 +11,10 @@ class monitoring extends ApplicationBase {
     public function __construct() {
         parent::__construct();
         // load model
+        $this->load->model('planning/m_planning');
+        $this->load->model('initiation/m_initiation');
+        $this->load->model('master/m_karyawan');
+        $this->load->model('master/m_client');
         // load library
         $this->load->library('tnotification');
         $this->load->library('datetimemanipulation');
@@ -23,7 +27,72 @@ class monitoring extends ApplicationBase {
         // set template content
         $this->smarty->assign("template_content", "monitoring/index.html");
         $this->smarty->assign('user', $this->com_user['user_id']);
+        $start = $this->uri->segment(4, 0) + 1;
+        $this->smarty->assign("no", $start);
         // output
         parent::display();
     }
+
+    function monitoring_process(){
+
+        $this->_set_page_rule("C");
+ 
+       
+            $params = array(
+                'id_initiation'     => $this->input->post('init_planning'),
+                'start_date'        => $this->input->post('start_planning'),
+                'due_date'          => $this->input->post('due_planning')
+                
+            );
+            $this->m_planning->insert_planning($params);
+            redirect("initiation/initiation/index");
+    }
+
+    function edit_process(){
+        // set page rules
+        $this->_set_page_rule("U");
+
+        // cek input
+        $this->tnotification->set_rules('id_planning', 'Nomor Identitas Planning', 'trim|required');
+        $this->tnotification->set_rules('project_title', 'Nama Project', 'trim|required');
+
+        if($this->tnotification->run() !== FALSE){
+            $params = array(
+                'id_department'     => $dep,
+                );
+            $where = array(
+                'id_planning' => $this->input->post('id_planning', TRUE),
+            );
+
+            if ($this->m_initiation->update_planning($params)) {
+                $this->tnotification->delete_last_field();
+                $this->tnotification->sent_notification("success", "Data berhasil disimpan");
+            }else{
+                // default error
+                $this->tnotification->sent_notification("error", "Data gagal disimpan");
+            }
+        }else{
+            // default error
+            $this->tnotification->sent_notification("error", "Data gagal disimpan");
+        }
+        // default redirect
+        redirect("planning/planning/edit/".$this->input->post('id_planning', TRUE));
+    }
+
+
+
+    function detail($where){
+        $this->_set_page_rule("U");
+        // set template content
+        $this->smarty->assign("template_content", "planning/detail.html");
+        
+       
+        $this->tnotification->display_notification();
+        $this->tnotification->display_last_field();
+
+        // output
+        parent::display();
+
+    }
+
 }
